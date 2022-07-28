84542b1cc4c7a5d1794aa722c0443e7e9a2fe290
DevopsTrainer2022 <gcpdevopsbatch10@gmail.com>
2022-07-28 07:24:36 +0530

    Created a file for multiplication

---
+++ Product.java
@@ -0,0 +1,14 @@
+class Product
+{
+	public static void main(String args[])
+	{
+		int n1,n2,res;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter n1 value");
+		n1=s.nextInt();
+		System.out.println("Enter n2 value");
+		n2=s.nextInt();
+		res=n1*n2;
+		System.out.println("The product value is : "+res);
+	}
+}
